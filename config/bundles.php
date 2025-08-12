b059e731ff2d4eca80b39bdf233896cd81bc4d3c
mtonzarsaphir <mtonzarsaphir@gmail.com>
2025-08-12 08:40:52 +0100

    commit

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-   
+
+    
+    mtonzar\SharedServicesBundle\SharedServicesBundle::class => ['all' => true],
 ];
