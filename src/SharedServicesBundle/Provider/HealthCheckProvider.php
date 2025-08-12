8d7cd36d8ab95e25e2d3cd34283ef568d7e118d3
mtonzarsaphir <mtonzarsaphir@gmail.com>
2025-08-12 11:03:30 +0100

    commit

--- src/SharedServicesBundle/Provider/HealthCheckProvider.php
+++ src/SharedServicesBundle/Provider/HealthCheckProvider.php
@@ -13,9 +13,9 @@ class HealthCheckProvider implements ProviderInterface
 {
     public function __construct(
         private DatabaseHealthChecker $databaseChecker,
-        private CacheHealthChecker $cacheChecker,
-        private QueueHealthChecker $queueChecker,
-        private ApiDependencyHealthChecker $apiDependencyChecker
+        // private CacheHealthChecker $cacheChecker,
+        // private QueueHealthChecker $queueChecker,
+        // private ApiDependencyHealthChecker $apiDependencyChecker
     ) {}
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): array
@@ -23,9 +23,9 @@ class HealthCheckProvider implements ProviderInterface
         $healthCheck = new HealthCheck();
 
         $healthCheck->addCheck('database', ...array_values($this->databaseChecker->check()));
-        $healthCheck->addCheck('cache', ...array_values($this->cacheChecker->check()));
-        $healthCheck->addCheck('queue', ...array_values($this->queueChecker->check()));
-        $healthCheck->addCheck('external_apis', ...array_values($this->apiDependencyChecker->check()));
+        // $healthCheck->addCheck('cache', ...array_values($this->cacheChecker->check()));
+        // $healthCheck->addCheck('queue', ...array_values($this->queueChecker->check()));
+        // $healthCheck->addCheck('external_apis', ...array_values($this->apiDependencyChecker->check()));
 
         return [$healthCheck];
     }
