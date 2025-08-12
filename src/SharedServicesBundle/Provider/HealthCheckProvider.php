ca534682fcf78e22d793a5da4af85d5cd0eebf7a
mtonzarsaphir <mtonzarsaphir@gmail.com>
2025-08-12 15:41:16 +0100

    commit

--- src/SharedServicesBundle/Provider/HealthCheckProvider.php
+++ src/SharedServicesBundle/Provider/HealthCheckProvider.php
@@ -8,6 +8,7 @@ use mtonzar\SharedServicesBundle\Service\HealthChecker\DatabaseHealthChecker;
 use mtonzar\SharedServicesBundle\Service\HealthChecker\CacheHealthChecker;
 use mtonzar\SharedServicesBundle\Service\HealthChecker\QueueHealthChecker;
 use mtonzar\SharedServicesBundle\Service\HealthChecker\ApiDependencyHealthChecker;
+use mtonzar\SharedServicesBundle\Service\HealthChecker\LivenessHealthChecker;
 
 class HealthCheckProvider implements ProviderInterface
 {
@@ -15,7 +16,8 @@ class HealthCheckProvider implements ProviderInterface
         private DatabaseHealthChecker $databaseChecker,
         // private CacheHealthChecker $cacheChecker,
         // private QueueHealthChecker $queueChecker,
-         private ApiDependencyHealthChecker $apiDependencyChecker
+         private ApiDependencyHealthChecker $apiDependencyChecker,
+         private LivenessHealthChecker $livenessChecker
     ) {}
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): array
@@ -26,6 +28,7 @@ class HealthCheckProvider implements ProviderInterface
         // $healthCheck->addCheck('cache', ...array_values($this->cacheChecker->check()));
         // $healthCheck->addCheck('queue', ...array_values($this->queueChecker->check()));
          $healthCheck->addCheck('external_apis', ...array_values($this->apiDependencyChecker->check()));
+         $healthCheck->addCheck('liveness', ...array_values($this->livenessChecker->check()));
 
         return [$healthCheck];
     }
