fa75b51af67abc16791e5d2b51fc1ece46ab4781
mtonzarsaphir <mtonzarsaphir@gmail.com>
2025-08-13 16:09:05 +0100

    commit

--- src/SharedServicesBundle/Provider/HealthCheckProvider.php
+++ src/SharedServicesBundle/Provider/HealthCheckProvider.php
@@ -5,28 +5,19 @@ use ApiPlatform\State\ProviderInterface;
 use ApiPlatform\Metadata\Operation;
 use mtonzar\SharedServicesBundle\Entity\HealthCheck;
 use mtonzar\SharedServicesBundle\Service\HealthChecker\DatabaseHealthChecker;
-use mtonzar\SharedServicesBundle\Service\HealthChecker\CacheHealthChecker;
-use mtonzar\SharedServicesBundle\Service\HealthChecker\QueueHealthChecker;
 use mtonzar\SharedServicesBundle\Service\HealthChecker\ApiDependencyHealthChecker;
 use mtonzar\SharedServicesBundle\Service\HealthChecker\LivenessHealthChecker;
-use mtonzar\SharedServicesBundle\Service\HealthChecker\ReadinessHealthChecker;
 
 class HealthCheckProvider implements ProviderInterface
 {
-    private DatabaseHealthChecker $databaseChecker;
-    private ApiDependencyHealthChecker $apiDependencyChecker;
-    private LivenessHealthChecker $livenessChecker;
     private array $services;
 
     public function __construct(
-        ?DatabaseHealthChecker $databaseChecker = null,
-        ?ApiDependencyHealthChecker $apiDependencyChecker = null,
-        ?LivenessHealthChecker $livenessChecker = null,
+        private ?DatabaseHealthChecker $databaseChecker = null,
+        private ?ApiDependencyHealthChecker $apiDependencyChecker = null,
+        private ?LivenessHealthChecker $livenessChecker = null,
         array $services = []
     ) {
-        $this->databaseChecker = $databaseChecker;
-        $this->apiDependencyChecker = $apiDependencyChecker;
-        $this->livenessChecker = $livenessChecker;
         $this->services = $services;
     }
 
@@ -34,37 +25,34 @@ class HealthCheckProvider implements ProviderInterface
     {
         $healthCheck = new HealthCheck();
 
-        $healthCheck->addCheck('database', ...array_values($this->databaseChecker->check()));
-        // $healthCheck->addCheck('cache', ...array_values($this->cacheChecker->check()));
-        // $healthCheck->addCheck('queue', ...array_values($this->queueChecker->check()));
-        $healthCheck->addCheck('readiness', ...array_values($this->readinessChecker->check()));
-         $healthCheck->addCheck('external_apis', ...array_values($this->apiDependencyChecker->check()));
-         $healthCheck->addCheck('liveness', ...array_values($this->livenessChecker->check()));
-        return [$healthCheck];
-    }
+        // Ancien mode
+        if ($this->databaseChecker) {
+            $healthCheck->addCheck('database', ...array_values($this->databaseChecker->check()));
+        }
+        if ($this->apiDependencyChecker) {
+            $healthCheck->addCheck('external_apis', ...array_values($this->apiDependencyChecker->check()));
+        }
+        if ($this->livenessChecker) {
+            $healthCheck->addCheck('liveness', ...array_values($this->livenessChecker->check()));
+        }
 
-    public function provideAll(Operation $operation, array $uriVariables = [], array $context = []): array
-    {
-        $healthCheck = new HealthCheck();
-
+        // Nouveau mode : ping vers microservices externes
         foreach ($this->services as $name => $url) {
-            $status = 'healthy';
-            $details = 'Ping OK';
-
-            try {
-                $response = @file_get_contents($url);
-                if ($response !== 'pong') {
-                    $status = 'degraded';
-                    $details = 'Ping response incorrect';
-                }
-            } catch (\Exception $e) {
-                $status = 'down';
-                $details = $e->getMessage();
-            }
-
-            $healthCheck->addCheck($name, $status, $details);
+            $status = $this->pingService($url) ? 'healthy' : 'down';
+            $healthCheck->addCheck($name, $status, "Ping $url returned $status");
         }
 
         return [$healthCheck];
+    }
+
+    private function pingService(string $url): bool
+    {
+        try {
+            $context = stream_context_create(['http' => ['timeout' => 2]]);
+            $result = @file_get_contents($url, false, $context);
+            return $result !== false;
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 }
